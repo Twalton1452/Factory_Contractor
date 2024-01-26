954ceacc4eada4cc1b61de9e24be59c4ce39210a
Tyler Walton <tyler.walton1452@gmail.com>
2024-01-26 13:02:58 -0600

    use more verbose names in inputhandler

--- scripts/player/PlayerInputHandler.gd
+++ scripts/player/PlayerInputHandler.gd
@@ -20,15 +20,10 @@ func _ready():
 	MessageBus.player_canceled.connect(_on_player_canceled)
 	MessageBus.player_rotated.connect(_on_player_rotated)
 	MessageBus.player_picked.connect(_on_player_picked)
-	disable()
+	exit_build_mode()
 
-func enable() -> void:
-	sprite.show()
-
-func disable() -> void:
-	sprite.hide()
-	sprite.rotation = 0.0
-	current_data = null
+func in_build_mode() -> bool:
+	return current_data != null
 
 func _on_build_slot_pressed(component_data: ComponentData) -> void:
 	enter_build_mode_with(component_data)
@@ -36,14 +31,19 @@ func _on_build_slot_pressed(component_data: ComponentData) -> void:
 
 func enter_build_mode_with(component_data: ComponentData) -> void:
 	current_data = component_data
-	enable()
 	if current_data:
+		sprite.show()
 		sprite.texture = component_data.icon
 		shape_cast.collision_mask = current_data.placed_layer
 		required_shape_cast.collision_mask = current_data.required_layer
 
+func exit_build_mode() -> void:
+	sprite.hide()
+	sprite.rotation = 0.0
+	current_data = null
+
 func _on_player_selected() -> void:
-	if current_data == null:
+	if not in_build_mode():
 		return
 	
 	var player_selected_position = placing_position(get_viewport().get_mouse_position())
@@ -62,9 +62,8 @@ func _on_player_selected() -> void:
 
 func _on_player_canceled() -> void:
 	# Cancel the Build if we aren't hovering anything
-	if current_data != null and not shape_cast.is_colliding():
-		sprite.hide()
-		current_data = null
+	if in_build_mode() and not shape_cast.is_colliding():
+		exit_build_mode()
 	# Remove things underneath what we're hovering
 	elif shape_cast.is_colliding():
 		var node = shape_cast.get_collider(0)
@@ -72,12 +71,14 @@ func _on_player_canceled() -> void:
 			node.queue_free()
 
 func _on_player_rotated() -> void:
-	if current_data != null:
+	if in_build_mode():
 		sprite.rotation += PI/2
+	
 	if shape_cast.is_colliding():
 		var node = shape_cast.get_collider(0)
 		if node is Building:
 			node.rotation += PI/2
+			sprite.rotation = node.rotation
 
 func _on_player_picked() -> void:
 	if shape_cast.is_colliding():
@@ -86,7 +87,7 @@ func _on_player_picked() -> void:
 			enter_build_mode_with(node.data)
 			sprite.rotation = node.rotation
 	else:
-		disable()
+		exit_build_mode()
 
 func placing_position(pos: Vector2) -> Vector2:
 	return Vector2(snapped(floor(pos.x), Constants.TILE_SIZE), snapped(floor(pos.y), Constants.TILE_SIZE))
@@ -98,7 +99,7 @@ func _physics_process(_delta):
 	elif shape_cast.is_colliding():
 		sprite.modulate = Color.RED
 		sprite.modulate.a = 0.5
-	elif current_data:
+	elif in_build_mode():
 		sprite.modulate = current_data.color_adjustment
 		sprite.modulate.a = 0.5
 	
